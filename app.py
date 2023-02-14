70206a05c77ad562f25e9d341ede9238e9a16e3f
ZeyXs <basile.gamer@gmail.com>
2023-02-14 13:51:26 +0100

    Fix sRGB error for all textures

--- app.py
+++ app.py
@@ -12,9 +12,7 @@ WIDTH, HEIGHT = 256, 224
 FPS = 60
 SCALING_FACTOR = 3
 
-
-
-
+# Application / Code du Jeu
 class App:
 
     def __init__(self):
@@ -25,26 +23,38 @@ class App:
         self.window = pygame.display.set_mode((WIDTH*SCALING_FACTOR, HEIGHT*SCALING_FACTOR))
         self.screen = pygame.Surface((WIDTH, HEIGHT))
 
+        # Importation des textures
+            # - Sprites
         self.player_img = pygame.image.load("./assets/textures/player.png").convert_alpha()
         self.crabe_0_img = pygame.image.load("./assets/textures/crabe_0.png").convert_alpha()
+        self.crabe_1_img = pygame.image.load("./assets/textures/crabe_1.png").convert_alpha()
+        self.meduse_0_img = pygame.image.load("./assets/textures/meduse_0.png").convert_alpha()
+        self.meduse_1_img = pygame.image.load("./assets/textures/meduse_1.png").convert_alpha()
+        self.poulpe_0_img = pygame.image.load("./assets/textures/poulpe_0.png").convert_alpha()
+        self.poulpe_1_img = pygame.image.load("./assets/textures/poulpe_1.png").convert_alpha()
+            # - Icon
         icon = pygame.image.load('./assets/textures/icon.png').convert_alpha()
+            # - Font
+        self.font = pygame.font.Font("./assets/fonts/space_invaders.ttf", 8)
 
+        # Esthétique fenêtre
         pygame.display.set_icon(icon)
         pygame.display.set_caption("Space Invaders v0.1")
-        self.clock = pygame.time.Clock()
-        self.running = True
-        
-        # Importation de la font
-        self.font = pygame.font.Font("./assets/fonts/space_invaders.ttf", 8)
 
         # Variables in-game
         self.in_main_menu = True
         self.enemies = []
+
+        # Clefs de contact (nom temporaire, ou pas...)
+        self.clock = pygame.time.Clock()
+        self.running = True
         
         # Création des entités
+            # - Joueur
         self.player = Player(gen_rect(WIDTH/2-(self.player_img.get_width()/2), 201, self.player_img), self.player_img)
-        for y in range(10, HEIGHT-50, 10):
-            for x in range(10, WIDTH-10, 10):
+            # - Ennemis
+        for y in range(30, HEIGHT-100, 12):
+            for x in range(30, WIDTH-100, 15):
                 self.enemies.append(Enemy(Type.CRABE, gen_rect(x, y, self.crabe_0_img), self.crabe_0_img, 5))
                 
     
@@ -53,10 +63,8 @@ class App:
         
         while self.running:
             self.clock.tick(FPS)
-
             self.keys_pressed = pygame.key.get_pressed()
             
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -65,14 +73,14 @@ class App:
                     if event.key == pygame.K_LEFT:
                         pass                
                     
-            self.dos()
+            self.draw_on_screen()
                     
         pygame.quit()
         sys.exit()
     
 
     # Update de la fenêtre - Draw on screen
-    def dos(self):
+    def draw_on_screen(self):
         self.screen.fill(BLACK)
 
         # Affichage du texte
